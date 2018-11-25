8f4ed50dd375959e37dc7bf383f4dfb95524393e
shaojunyan <635178917@qq.com>
2018-11-25 13:18:19 +0800

    商品详情

--- src/pages/goodsDetail/index.vue
+++ src/pages/goodsDetail/index.vue
@@ -8,39 +8,48 @@
         </ul>
         <swiper class="swiper-container" :current="activeItem" duration="300" @change="switchTabBySwiper" :style="{height:winHeight+'rpx'}" skip-hidden-item-layout="true">
             <swiper-item>
-                <scroll-view scroll-y="true">
+                <scroll-view scroll-y="true" :style="{height:winHeight+'rpx'}">
                     <section class="header">
                         <swiper class="goodsimgs" indicator-dots="true" autoplay="true" interval="3000" duration="1000">
                             <swiper-item v-for="(item, index) of goodsDetail.gallery" :key="index" :data-index="index">
-                                <img :src="item" background-size="cover"/>
+                                <img class="image" :src="item" background-size="cover"/>
                             </swiper-item>
                         </swiper>
-                        <div>{{goodsDetail.name}}</div>
-                        <div>
-                            <div>￥{{goodsDetail.rentPrice}}/{{goodsDetail.periodUnit}}</div>
-                            <div>商品价值：￥{{goodsDetail.retailPrice}}</div>
+                        <div class="goods-title">{{goodsDetail.name}}</div>
+                        <div class="goods-price-container">
+                            <div class="rent-price">￥<span class="big-price">{{goodsDetail.rentPrice}}</span><span class="normal-font">/{{goodsDetail.periodUnit}}</span></div>
+                            <div class="total-price">商品价值：￥{{goodsDetail.retailPrice}}</div>
                         </div>
                     </section>
                     <section>
-                        <div>租赁流程</div>
-                        <div>选择商品 -- 下单审核 -- 首期支付 -- 发货 -- 月付租金 -- 归还商品</div>
-                        <div>租期结束后可选方案</div>
-                        <div>归还</div>
-                        <div>支持邮寄归还</div>
-                        <div>买断</div>
-                        <div>支持尾款终身拥有</div>
+                        <div class="zuling-header">租赁流程</div>
+                        <div class="zuling-content">选择商品 -- 下单审核 -- 首期支付 -- 发货 -- 月付租金 -- 归还商品</div>
+                        <div class="zuling-header">租期结束后可选方案</div>
+                        <div class="zuling-subheader">归还</div>
+                        <div class="zuling-content">支持邮寄归还</div>
+                        <div class="zuling-subheader">买断</div>
+                        <div class="zuling-content">支持尾款终身拥有</div>
+                    </section>
+                    <section>
+                        <div class="zuling-header">其他说明</div>
+                        <div class="zuling-subheader">收货须知</div>
+                        <div class="zuling-content">身份证正反面复印件以确认本人收货，并交由快递小哥寄回本平台制定地点存档</div>
+                        <div class="zuling-subheader">租金</div>
+                        <div class="zuling-content">用户需按月支付租金，需开通支付宝/微信免密制度协议，平台每月将自动从支付宝/微信账号中扣除相应租金。用户也可提前主动支付剩下的租金</div>
+                        <div class="zuling-subheader">买断</div>
+                        <div class="zuling-content">租赁到期后可支付买断款买断该商品，买断款以下单时协议里的买断价为准。</div>
                     </section>
                 </scroll-view>
             </swiper-item>
             <swiper-item>
-                <scroll-view scroll-y="true">
-                    <div>
-                    222222222222222
+                <scroll-view scroll-y="true" :style="{height:winHeight+'rpx'}">
+                    <div class="img-container">
+                        <img v-for="(item, index) of goodsDetail.description" :key="index" :data-index="index" class="desc-image" :src="item" background-size="cover"/>
                     </div>
                 </scroll-view>
             </swiper-item>
             <swiper-item>
-                <scroll-view scroll-y="true">
+                <scroll-view scroll-y="true" :style="{height:winHeight+'rpx'}">
                     <div>
                     33333333333333333333333
                     </div>
@@ -106,18 +115,13 @@
             </view>
         </view>
         </view> -->
-        <!-- <view class="bottom-btn">
-            <view class="l l-collect" @click="addCannelCollect">
-                <img class="icon" :src="collectBackImage"/>
+        <view class="bottom-btn">
+            <view class="l l-collect" >
+                <img class="icon"/>
+                收藏
             </view>
-            <view class="l l-cart">
-                <view class="box">
-                    <text class="cart-count">{{cartGoodsCount}}</text>
-                    <img @click="openCartPage" class="icon" src="/static/images/ic_menu_shoping_nor.png"/>
-                </view>
-            </view>
-            <view class="c">立即购买</view>
-        </view> -->
+            <view class="c">选择规格</view>
+        </view>
     </view>
 </div>
 </template>
@@ -163,19 +167,20 @@
                         const clientHeight = res.windowHeight
                         const clientWidth = res.windowWidth
                         const rpxR = 750 / clientWidth;
-                        const calc = clientHeight * rpxR - 100;
+                        const calc = clientHeight * rpxR - 170;
                         this.winHeight = calc
                     }
                 });
             } else {
                 my.getSystemInfo({
                     success: res => {
-                        console.log(res)
                         const clientHeight = res.windowHeight
                         const clientWidth = res.windowWidth
                         const rpxR = 750 / clientWidth;
-                        const calc = clientHeight * rpxR - 100;
+                        const calc = clientHeight * rpxR - 155;
                         this.winHeight = calc
+                        console.log(clientHeight)
+                        console.log(this.winHeight)
                     }
                 });
             }
@@ -223,44 +228,121 @@
     .icon-favor:before { content: "\e64c"; }
     .tab-list {
         display: flex;
-        height: 50px;
+        height: 70rpx;
         justify-content: space-around;
         align-items: center;
-        width: 90%;
+        width: 50%;
+        margin-left: 25%;
     }
-    .tab-list li {
-        font-size: 14px;
-        line-height: 40px;
+    .tab-list view {
+        font-size: 28rpx;
+        font-weight: 700;
+        line-height: 35rpx;
         color: #a8a8a8;
-        border-radius: 25px;
-        width: 100px;
+        // border-radius: 25px;
+        // width: 100px;
         display: flex;
         justify-content: center;
-        border: 0.5px solid #FFFFFF;
+        padding-right: 8rpx;
+        padding-bottom: 5rpx;
+        // border: 0.5px solid #FFFFFF;
     }
     .tab-list .active {
-        border: 0.5px solid #f6f6f6;
-        box-shadow: 2px 2px 10px #cccccc;
+        border-bottom: 5rpx solid #1a1717;
+        // box-shadow: 2px 2px 10px #cccccc;
         color:#1b1b1b;
     }
-    .swiper-container {
-        width: 94%;
+    .header {
+        width: 692rpx;
+        // height: 700rpx;
+        margin-left: 29rpx;
+        border: 1px solid #FAFAFA;
+        border-radius: 20rpx;
+        margin-top: 20rpx;
+        box-shadow: 2rpx 2rpx 50rpx #cccccc;
     }
-    .container {
-        margin-bottom: 100rpx;
-    }
-
     .goodsimgs {
-        width: 750rpx;
-        height: 750rpx;
+        width:450rpx;
+        height: 450rpx;
+        margin-left: 121rpx;
+        margin-top: 20rpx;
     }
-
-    .goodsimgs image {
-        width: 750rpx;
-        height: 750rpx;
-        background-color: #eee;
+    .goodsimgs .image{
+        width: 450rpx;
+        height: 450rpx;
+        // height: 100%;
     }
-
+    .goods-title {
+        font-weight: 700;
+        margin-left: 30rpx;
+        margin-top: 20rpx;
+    }
+    .goods-price-container {
+        display: flex;
+        // margin-top: 20rpx;
+        justify-content: space-between;
+        padding: 0rpx 30rpx;
+    }
+    .rent-price {
+        font-size: 24rpx;
+        color:#FF6900;
+        // line-height: 30rpx;
+    }
+    .rent-price .big-price {
+        font-size: 50rpx;
+    }
+    .rent-price .normal-font {
+        color: #a8a8a8;
+    }
+    .total-price {
+        font-size: 24rpx;
+        color: #a8a8a8;
+        margin-top: 40rpx;
+        // line-height: 45rpx;
+    }
+    // --ww
+    // .total-price {
+    //     font-size: 24rpx;
+    //     color: #a8a8a8;
+    //     margin-top: 30rpx;
+    //     // line-height: 45rpx;
+    // }
+    .zuling-header {
+        font-size: 28rpx;
+        font-weight: 700;
+        margin-left: 29rpx;
+        margin-top: 40rpx;
+    }
+    .zuling-subheader {
+        font-size: 26rpx;
+        font-weight: 400;
+        margin-left: 29rpx;
+        margin-top: 40rpx;
+    }
+    .zuling-content {
+        font-size: 26rpx;
+        margin-left: 29rpx;
+        margin-right: 29rpx;
+        margin-top: 25rpx;
+        color: #a8a8a8;
+        line-height: 50rpx;
+    }
+    .bottom-btn {
+        width: 100%;
+        height: 100rpx;
+        position: fixed;
+        bottom: 0;
+        background-color: #FF6900
+    }
+    .img-container {
+        margin-top: 20rpx;
+        width: 750rpx;
+    }
+    .desc-image {
+        width: 750rpx;
+        margin-top:-8rpx;
+        height: 900rpx;
+    }
 
     
 </style>
